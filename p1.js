3f620dcabe9ca92f525d59a67f1bf851fb5f4abf
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-17 20:57:43 +0600

    jhalai js

---
+++ p1.js
@@ -0,0 +1,18 @@
+// function billAmount(mainBill, tipPercentage) {
+//     let tipamount = (mainBill * tipPercentage) / 100;
+//     let totalAmount = mainBill + tipamount;
+//     return totalAmount;
+// }
+
+// let myBill = billAmount(1000,5);
+// console.log(myBill);
+
+
+let age = 16;
+let text = (age >= 16) ? "Adult" : "Minor";
+
+console.log(text);
+
+
+
+

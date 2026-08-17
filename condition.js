52669b69254aed83ac29830c14bac8abef79b8d8
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-17 21:32:31 +0600

    loop practice

---
+++ condition.js
@@ -0,0 +1,21 @@
+// Using If-else Method
+let budget = 14000;
+let text = "going to sajek";
+if(budget >= 15000)
+{
+    text = "goint to Cox's bazar";
+}
+
+else if(budget <= 10000)
+{ 
+    text = "going to sylhet";
+}
+
+
+console.log(text);
+
+
+// // Using ternary method
+// let budget = 1000;
+// let text = (budget >= 15000) ? "Going Chittagong" : "Going to Sajek";
+// console.log(text);

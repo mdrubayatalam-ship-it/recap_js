a93cb590ced31f9ebf4161b563a3b93bb48d19d1
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-20 10:32:51 +0600

    DOM porichiti

---
+++ DOM.js
@@ -0,0 +1,7 @@
+// DOM means Document Object Model 
+// Each part of Document are nodes
+// Most Common method to access an element is to use id.
+
+const myPara = document.getElementById("Practice");
+myPara.innerText = "Rubayat";
+myPara.style.backgroundColor
--- function.js
+++ function.js
@@ -1,7 +1,8 @@
-function impedance(frequency , inductance)
-{
-    let imp = 2 * 3.1416 * frequency * inductance ;
-    return imp;
-}
 
-console.log(impedance(1000,0.1));
+// function impedance(frequency , inductance)
+// {
+//     let imp = 2 * 3.1416 * frequency * inductance ;
+//     return imp;
+// }
+// console.log(impedance(1000,0.1));
+
